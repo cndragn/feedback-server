262f0a16dcc4500a8d2df5789bc815c04e0ce8fb
Candice Davidson <cndragn@gmail.com>
2018-12-16 02:06:28 -0800

    create compdidmount to fetch surveys api

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions';
 
 class SurveyList extends Component {
+	componentDidMount() {
+		this.props.fetchSurveys();
+	}
 	render() {
 		return <div>SurveyList</div>;
 	}
