c5f0157cb97c64d2c653590e7c1cb4840ea2b9be
Candice Davidson <cndragn@gmail.com>
2018-12-16 11:45:11 -0800

    update webhook to test sendgrid in prod

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -23,6 +23,7 @@ module.exports = (app) => {
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
+		console.log(res.body);
 		const p = new Path('/api/surveys/:surveyId/:choice');
 		_.chain(req.body)
 			.map(({ email, url }) => {
